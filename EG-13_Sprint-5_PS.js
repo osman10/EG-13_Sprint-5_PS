795d4067e8dc9fc6db37160c92cd29ac27fa5aca
Md. Osman Goni <osman.mirpur55@gmail.com>
2026-09-20 19:15:35 +0600

    Implement various algorithms in EG-13_Sprint-5_PS.js

--- EG-13_Sprint-5_PS.js
+++ EG-13_Sprint-5_PS.js
@@ -1,1 +1,246 @@
+// ==========================================
+// EG-13_Sprint-5_PS.js
+// ==========================================
 
+/**
+ * 01. Remove Duplicates from Sorted Array
+ * Note: The prompt boilerplate signature had incorrect parameters (checkSubarraySum),
+ * but the canonical LeetCode function for this problem is removeDuplicates(nums).
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if (nums.length === 0) return 0;
+    let k = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[k] = nums[i];
+            k++;
+        }
+    }
+    return k;
+};
+
+/**
+ * 02. Binary Search
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+};
+
+/**
+ * 03. Search Insert Position
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return left;
+};
+
+/**
+ * 04. Maximum Depth of Binary Tree
+ * 
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if (!root) return 0;
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+};
+
+/**
+ * 05. Invert Binary Tree
+ * 
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+    if (!root) return null;
+    
+    let temp = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+    
+    return root;
+};
+
+/**
+ * 06. Product of Array Except Self
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const n = nums.length;
+    const res = new Array(n);
+
+    // Calculate left products
+    res[0] = 1;
+    for (let i = 1; i < n; i++) {
+        res[i] = res[i - 1] * nums[i - 1];
+    }
+
+    // Multiply by right products
+    let rightProduct = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        res[i] = res[i] * rightProduct;
+        rightProduct *= nums[i];
+    }
+
+    return res;
+};
+
+/**
+ * 07. Rotate Array
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void}
+ */
+var rotate = function(nums, k) {
+    k = k % nums.length;
+    
+    const reverse = (start, end) => {
+        while (start < end) {
+            [nums[start], nums[end]] = [nums[end], nums[start]];
+            start++;
+            end--;
+        }
+    };
+
+    reverse(0, nums.length - 1);
+    reverse(0, k - 1);
+    reverse(k, nums.length - 1);
+};
+
+/**
+ * 08. Min Stack
+ */
+var MinStack = function() {
+    this.stack = [];
+    this.minStack = [];
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val);
+    if (this.minStack.length === 0 || val <= this.minStack[this.minStack.length - 1]) {
+        this.minStack.push(val);
+    } else {
+        this.minStack.push(this.minStack[this.minStack.length - 1]);
+    }
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop();
+    this.minStack.pop();
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1];
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.minStack[this.minStack.length - 1];
+};
+
+/**
+ * 09. Continuous Subarray Sum
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    // Stores modulo mapped to first seen index
+    const remainderMap = new Map();
+    remainderMap.set(0, -1); // To handle subarrays starting from index 0
+    
+    let runningSum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        runningSum += nums[i];
+        let remainder = runningSum % k;
+        
+        // Handle negative remainders if any (not strictly needed for non-negative array inputs)
+        if (remainder < 0) remainder += k;
+
+        if (remainderMap.has(remainder)) {
+            if (i - remainderMap.get(remainder) >= 2) {
+                return true;
+            }
+        } else {
+            remainderMap.set(remainder, i);
+        }
+    }
+
+    return false;
+};
+
+/**
+ * 10. Daily Temperatures
+ * 
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+var dailyTemperatures = function(temperatures) {
+    const res = new Array(temperatures.length).fill(0);
+    const stack = []; // Monotonic decreasing stack storing indices
+
+    for (let i = 0; i < temperatures.length; i++) {
+        while (stack.length > 0 && temperatures[i] > temperatures[stack[stack.length - 1]]) {
+            const prevIndex = stack.pop();
+            res[prevIndex] = i - prevIndex;
+        }
+        stack.push(i);
+    }
+
+    return res;
+};
